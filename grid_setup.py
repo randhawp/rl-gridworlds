c7303535b777f19fe2cbaaf0d27f973b30e6ffa1
pr <randhawa.puneet@gmail.com>
2020-05-17 13:19:31 -0400

    Added gridworld environment

---
+++ grid_setup.py
@@ -0,0 +1,83 @@
+
+
+import time
+import argparse
+import numpy as np
+import gym
+import gym_minigrid
+from gym_minigrid.wrappers import *
+from gym_minigrid.window import Window
+
+def redraw(img):
+    img = env.render('rgb_array', tile_size=10)
+    window.show_img(img)
+
+def reset():
+    obs = env.reset()
+
+    if hasattr(env, 'mission'):
+        print('Mission: %s' % env.mission)
+        window.set_caption(env.mission)
+
+    redraw(obs)
+
+def step(action):
+    obs, reward, done, info = env.step(action)
+    print('step=%s, reward=%.2f' % (env.step_count, reward))
+
+    if done:
+        print('done!')
+        reset()
+    else:
+        redraw(obs)
+
+def key_handler(event):
+    print('pressed', event.key)
+
+    if event.key == 'escape':
+        window.close()
+        return
+
+    if event.key == 'backspace':
+        reset()
+        return
+
+    if event.key == 'left':
+        step(env.actions.left)
+        return
+    if event.key == 'right':
+        step(env.actions.right)
+        return
+    if event.key == 'up':
+        step(env.actions.forward)
+        return
+
+    # Spacebar
+    if event.key == ' ':
+        step(env.actions.toggle)
+        return
+    if event.key == 'pageup':
+        step(env.actions.pickup)
+        return
+    if event.key == 'pagedown':
+        step(env.actions.drop)
+        return
+
+    if event.key == 'enter':
+        step(env.actions.done)
+        return
+
+
+
+env = gym.make('MiniGrid-Empty-5x5-v0')
+
+env = RGBImgPartialObsWrapper(env)
+env = ImgObsWrapper(env)
+
+window = Window('gym_minigrid - ')
+window.reg_key_handler(key_handler)
+
+reset()
+
+# Blocking event loop
+window.show(block=True)
