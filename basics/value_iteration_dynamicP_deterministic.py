55353e02321ecd91fb482ecdc0c7bb4bf4cfe42e
puneet randhawa <randhawa.puneet@gmail.com>
2020-05-23 02:38:43 +0000

    value iteration deterministic discreet space

--- basics/value_iteration_dynamicP_deterministic.py
+++ basics/value_iteration_dynamicP_deterministic.py
@@ -3,39 +3,36 @@ VALUE ITERATION
 - Synchronous Dynamic programming - that is sweep across all states
 - Deterministic actions - i.e all possible actions have equal probability
 
-This example illustrates value iteration with with the help of a [ n x n ] grid
-The top left and bottom right are terminal states with value 0
-The agent is following a uniform random policy i.e all its actions have equal probability
-Reward = -1
-Gamma = 1
-Two terminal states with value 0
+
+Reward = -0.04 for non-termianl states
+Gamma = 0.9
+Two terminal states with value +1 and -1
 The grid starts with all cells of the grid initialzed as zero and the terminal state remain zero
-+---+--+--+---+
-| 0 |  |  |   |
-+---+--+--+---+
-|   |  |  |   |
-+---+--+--+---+
-|   |  |  |   |
-+---+--+--+---+
-|   |  |  | 0 |
-+---+--+--+---+
+
++--+--+--+----+
+|  |  |  | +1 |
++--+--+--+----+
+|  |  |  | -1 |
++--+--+--+----+
+|  |  |  |    |
++--+--+--+----+
 
 '''
 
 import numpy as np 
 
 # the board size can be changed
-BOARD_ROW = 4
+BOARD_ROW = 3
 BOARD_COL = 4
 
-START_ROW=0
+START_ROW=2
 START_COL=0
 current_row=START_ROW
 current_col=START_COL
 
 
-# trow, tcol are x,y for the bottom right terminal states
-trow = BOARD_ROW -1 
+# trow, tcol are x,y for the top right
+trow = 0
 tcol = BOARD_COL - 1
 
 #init the states grid
@@ -46,8 +43,8 @@ states = np.zeros(BOARD_ROW*BOARD_COL).reshape(BOARD_ROW,BOARD_COL)
 #this is to store the cumulative value after the sweep ends
 statesnew = np.zeros(BOARD_ROW*BOARD_COL).reshape(BOARD_ROW,BOARD_COL)
 
-states[trow,tcol]=0 # terminal state value   
-states[0,0]=0 # terminal state value
+states[trow,tcol]= 1 # terminal state value   
+states[trow+1,tcol]= -1 # terminal state value
 
 '''
 Note on actions
@@ -59,45 +56,67 @@ The sum of probabilites of all actions has to be 100%
 
 Each action has a reward of -1
 '''
+def getreward(row,col):
+  reward = -0.04
+  if row==trow and col == tcol:
+      reward=1
+  if row==trow+1 and col==tcol:
+      reward=-1
+  return reward
 
-gamma=1
-reward=-1 
+gamma=0.9
+reward=0 
 print(states)
-t=0
-convergencelimit = 0.0001
-converged = False
 
+t=0
+convergencelimit = 0.001
+converged = False
+c=4 #can start from any cell 0 is first (top left)
 while not converged :
   row=0
   col=0
-  c=1
+  
   temp=0
-  while c < (BOARD_ROW * BOARD_COL -1 ): #loop through all the cells of grid except first and last i.e 0 and 15
-    row=int(c/BOARD_ROW)
+  while c < (BOARD_ROW * BOARD_COL  ): #loop through all the cells of grid 
+    row=int(c/BOARD_COL)
     col=c-(row*BOARD_COL)
     
     #for each cell / state caluate the value for each action
     actionvalue=np.zeros(4)
+
+    if row==trow and col==tcol:
+      c=c+1
+      statesnew[trow,tcol]= 1 # terminal state value   
+      continue
+    if row==trow+1 and col==tcol:
+      statesnew[trow+1,tcol]= -1 # terminal state value   
+      c=c+1
+      continue
+      
+    
     if row - 1 >=0: #top
-      actionvalue[0]=0.25 * (reward + gamma*(states[row-1,col]))
+      actionvalue[0]=0.25 * (getreward(row-1,col) + gamma*(states[row-1,col]))
     else:
-      actionvalue[0]=0.25 *  (reward + gamma*(states[row,col]))
+      actionvalue[0]=0.25 *  (getreward(row,col) + gamma*(states[row,col]))
     
     if row + 1 < BOARD_ROW:#bottom
-      actionvalue[1]=0.25 * (reward + gamma*(states[row+1,col]))
+      actionvalue[1]=0.25 * (getreward(row+1,col) + gamma*(states[row+1,col]))
     else:
-      actionvalue[1]=0.25 * (reward + gamma*(states[row,col]))
+      print( "row col",row,col)
+      actionvalue[1]=0.25 * (getreward(row,col) + gamma*(states[row,col]))
     
     if col - 1 >= 0:#left
-      actionvalue[2]=0.25* (reward + gamma*(states[row,col-1]))
+      actionvalue[2]=0.25* (getreward(row,col-1) + gamma*(states[row,col-1]))
     else:
-      actionvalue[2]=0.25 * (reward + gamma*(states[row,col]))
+      actionvalue[2]=0.25 * (getreward(row,col) + gamma*(states[row,col]))
     
     if col + 1 < BOARD_COL: #right
-      actionvalue[3]=0.25* (reward + gamma*(states[row,col+1]))
+      actionvalue[3]=0.25* (getreward(row,col+1) + gamma*(states[row,col+1]))
     else:
-      actionvalue[3]=0.25 * (reward + gamma*(states[row,col]))
-      
+      actionvalue[3]=0.25 * (getreward(row,col) + gamma*(states[row,col]))
+
+
+    reward = 0
     # add up the value for each of the possible actions in the current
     # cell and update statesnew (Note: do not update the current grid values
     # until full sweep of the entire state space is done)
@@ -113,7 +132,8 @@ while not converged :
   #after each pass update the state space with the new values  
   states = np.copy(statesnew)
   t=t+1  
-  print(np.round(statesnew))
+  c=0
+  print((states))
 
   #have the states converged (exit condition)
   if(diffav <= convergencelimit):
@@ -122,16 +142,11 @@ while not converged :
 print("Convergence reached after ", t , " steps")
 
 '''
-On convergence
-+-----+-----+-----+-----+
-|  0  | -14 | -20 | -22 |
-+-----+-----+-----+-----+
-| -14 | -18 | -20 | -20 |
-+-----+-----+-----+-----+
-| -20 | -20 | -18 | -14 |
-+-----+-----+-----+-----+
-| -22 | -20 | -14 |  0  |
-+-----+-----+-----+-----+
+[[-0.30734218 -0.17750818  0.31816232  1.        ]
+ [-0.40129651 -0.44877186 -0.7068359  -1.        ]
+ [-0.45373959 -0.53556325 -0.76908029 -1.23208465]]
+
+Convergence reached after  27  steps
 
 '''
 
