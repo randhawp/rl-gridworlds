65fa6b59140bc33d1c86f6acd2a7bd11873f67b5
puneet randhawa <randhawa.puneet@gmail.com>
2020-05-28 20:07:52 +0000

    modified the policy function

--- gym/frozen3_policy_iteration_eval.py
+++ gym/frozen3_policy_iteration_eval.py
@@ -18,21 +18,45 @@ custom_map = [
 ]
 # in above S can be anywhere there is a F. Only one S though
 
+'''
+Modified argmax to return location of all highest values
+'''
+def findargmax( arr ):
+    x = np.max(arr) #find highest 
+    y = np.where(arr==x) #find index of 1 or n highest
+    return  y[0].flatten().tolist(),len(y[0])
+
+
+'''
+Policy function gets the statevalue array (precalculated due to value iteration)
+and then uses those values to return an array that has the state/action pairs
+i.e direction to take from every state. 0/1/2/3 correspond to direction to take
+A direction of -1 (no direction) is added for the terminal states
+LEFT = 0
+DOWN = 1
+RIGHT = 2
+UP = 3
+'''
 def evaluate_policy(env,statevalue):
   i=0
   neighbourvalues=np.zeros(4)
-  policy=np.zeros(16)
+  policy=[]
   while i < env.observation_space.n:
     j=0
     while j< env.action_space.n:
       nextstate = env.P[i][j][0][1]
-      print(nextstate)
       neighbourvalues[j]=statevalue[nextstate]
-      j=j+1      
-    
-    policy[i]=np.argmax(neighbourvalues)
+      j=j+1    
+      
+    directions,length = findargmax(neighbourvalues)
+    if length==4:
+      policy.append(-1)
+    else:
+      policy.append(directions)
     i=i+1
   return policy
+
+
 
 gamma=1.0  #discount factor
 reward=0
@@ -55,8 +79,8 @@ Continue till the time convergence is achieved.
 '''
 i=j=0
 
-v=np.zeros(16)  # holds the actual values
-vtemp=np.zeros(16) # holds values temporarily until sweep is finished
+v=np.zeros(16)  # holds the actual values for 4x4 states
+vtemp=np.zeros(16) # holds values temporarily until a sweep of 4x4 states is finished
 actionvalue=np.zeros(4) # array to store the value for a state due to actions in that state
 convergencelimit = 0.0001
 converged = False
@@ -74,11 +98,12 @@ while not converged:
       else:
         reward=-1
       p=0.25 # override probability distribution and set every action to equal chance
+      # 0.25 = 1 / (no of possible actions)
       
       actionvalue[j] = p * (reward + gamma*v[nextstate]) # value of this state for this action
       j=j+1
 
-    vtemp[i] = np.sum(actionvalue) # value is the sum of all action value
+    vtemp[i] = np.sum(actionvalue).round(5) # value is the sum of all action value
     i=i+1
     
   #check if converged
@@ -96,4 +121,8 @@ print(v.reshape(4,4))
 print("------------------")
 print("From the above state values we can find the policy")
 policy = evaluate_policy(env,v)
-print(policy.reshape(4,4))
+#printing policy array in sections to reshape it
+print(policy[0:4])
+print(policy[4:8])
+print(policy[8:12])
+print(policy[12:16])
